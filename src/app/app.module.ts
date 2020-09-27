002d46bbee44ead27c70e02acf719935c47a48f1
luismila <luismilaroca@gmail.com>
2020-09-27 05:30:11 -0400

    Creacion componente error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { AccessDeniedComponent } from './components/access-denied/access-denied.component';
+import { ErrorComponent } from './components/error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AccessDeniedComponent
+    AccessDeniedComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
