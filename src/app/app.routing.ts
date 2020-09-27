56edcb2853619d3e0bfb7451b10d626906327c58
luismila <luismilaroca@gmail.com>
2020-09-27 05:35:59 -0400

    inclucion de los path acces-denied, error y not found en el app.routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AccessDeniedComponent } from "./components/access-denied/access-denied.component"
-
+import { ErrorComponent } from "./components/error/error.component"
+import { NotFoundComponent } from "./components/not-found/not-found.component"
 
 const routes: Routes = [
-  { path: "access-denied", component: AccessDeniedComponent}
+  { path: "access-denied", component: AccessDeniedComponent},
+  { path: "error", component: ErrorComponent},
+  { path: "not-found", component: NotFoundComponent}
 ];
 
 @NgModule({
